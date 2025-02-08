d33fabb4c6860513fdb9a2600d2867079fe5a36e
Andrew Coffman <tuq12108@temple.edu>
2025-02-07 23:57:30 -0500

    added scaffolding for player class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,20 @@
 
 public class Player {
+    private double balance;
 
+    public Player(){
+        balance = 100000;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void buyStock(double price){
+        balance -= price;
+    }
+
+    public void sellStock(double price){
+        balance += price;
+    }
 }
